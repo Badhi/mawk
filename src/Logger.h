8ea4afbcb1b0021a0c6e9684d6bf078c67886343
Bhashitha Adhikarinayake <bmadhikarinayake@gmail.com>
2020-04-11 20:50:53 +0530

    Crashes at bad value on boost any

---
+++ src/Logger.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define LOG(message) \
+    std::cout << message << std::endl;
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include <fstream>
 #include <sstream>
 
+#include "Logger.h"
+
 namespace po = boost::program_options;
 
 int main(int argc, char ** argv)
@@ -12,41 +14,50 @@ int main(int argc, char ** argv)
     po::options_description desc("Allowed options");
 
     desc.add_options()
-        ("field-separator,F", po::value<char>(), "Field separator")
+        ("field-separator,F", po::value<char>()->default_value(' '), "Field separator")
         ("file,f", po::value<std::string>(), "Script name")
         ("val,v", po::value<std::vector<std::string>>()->multitoken(), "Followed with var=value, assigns value to var in the script")
-        ("help", "help") 
-    ;
+        ("help", "help")
+        ;
 
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm);    
-
-    if (vm.count("help")) {
-        std::cout << desc << "\n";
-        return 1;
-    }
-
-    char fieldSeparator = ' ';
-    if (vm.count("F")) {
-        fieldSeparator = vm["F"].as<char>();
-    }
-
+    char fieldSeparator;
     std::string phrase;
-    if(vm.count("f")){
-        std::string script_file_name = vm["f"].as<std::string>();
-        std::ifstream script_file(script_file_name);
-        std::stringstream s;
-        if(!script_file.is_open()){
-            std::cerr << "Unable to open script file : " << script_file_name << std::endl; 
+
+    try {
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+
+        if (vm.count("help")) {
+            std::cout << desc << "\n";
             return 1;
         }
-        s << script_file.rdbuf();
-        phrase = s.str();
-    } 
-    else{
-        phrase = argv[argc-1];
-    } 
+
+        fieldSeparator = vm["F"].as<char>();
+
+        LOG("Field seperator : " <<  fieldSeparator);
+
+        std::string script_file_name = vm["f"].as<std::string>();
+
+        if(!script_file_name.empty()) {
+            LOG("Script file found : " <<  script_file_name);
+
+            std::ifstream script_file(script_file_name);
+            std::stringstream s;
+            if(!script_file.is_open()){
+                std::cerr << "Unable to open script file : " << script_file_name << std::endl;
+                return 1;
+            }
+            s << script_file.rdbuf();
+            phrase = s.str();
+        }
+        else{
+            phrase = argv[argc-1];
+        }
+    } catch(boost::wrapexcept<po::unknown_option> & ex) {
+        std::cerr << "Failed " << ex.what() << std::endl;
+        return 1;
+    }
 
     std::cout << "Field separator : " << fieldSeparator << std::endl;
     std::cout << "Phrase : " << std::endl;
